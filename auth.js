255cf7be30840a3a6b950a9eab58dfb362fadef7
DARKENIST <stepanhunter2007@gmail.com>
2025-10-16 13:59:03 +0700

    Add login function

--- auth.js
+++ auth.js
@@ -1,1 +1,21 @@
 
+const users = [ 
+  { id: 1, username: "admin", password: "StrashilkaIS", email: "adminka@example.com" },
+  { id: 2, username: "user1", password: "user2007", email: "userka@example.com" }
+  ];
+
+let currentUser = null;
+
+function login(username, password) {
+  const user = users.find( u => u.username === username && u.password === password);
+
+if (user) {
+  currentUser = user;
+  console.log('Welcome, ${user.username}');
+  updateUI();
+  return true;
+} else {
+  console.log("Incorrect Data");
+  return false;
+}
+}
